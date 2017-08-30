f602f6d3e325c50bb45041812e6e5b9f576518a9
hyalen.moreira@gmail.com <hyalen.moreira@gmail.com>
2017-08-29 17:49:04 -0700

    Remove comment code

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -14,41 +14,15 @@ class SearchBooks extends Component {
     this.setState({query : event.target.value})
   }
 
-  handleSelectChange = (selected, book) => {
-    this.setState({ selected })
-
-    if (this.props.onUpdateBooks) {
-      for (let obj of this.props.books) {
-        if (obj.id === book.id) {
-          obj.shelf = selected
-          this.props.onUpdateBooks(this.props.books)
-
-          BooksAPI.update(book, selected)
-          //   .then(
-          //     this.props.onUpdateBooks(book)
-          // )
-        }
-      }
-    }
-  }
-
   handleSubmit = (event) => {
     event.preventDefault()
 
     BooksAPI.search(this.state.query, 10)
-      .then((booksSearched) => {
-        this.setState({booksSearched})})
+      .then(booksSearched => this.setState({booksSearched}))
       .catch((e) => {this.setState({booksSearched : []})})
   }
 
-  // componentDidMount() {
-  //   BooksAPI.getAll().then((booksSearched) => {
-  //     this.setState({booksSearched})
-  //   })
-  // }
-
   render () {
-    const { books, onUpdateBooks } = this.props
     const { query, booksSearched } = this.state
 
     return (
@@ -66,33 +40,7 @@ class SearchBooks extends Component {
           </div>
         </div>
         <div className="search-books-results">
-          {/*<ListBooksGrid books={ booksSearched } onUpdateBooks={ onUpdateBooks }></ListBooksGrid> */}
-          <ol className="books-grid">
-            {
-            booksSearched.map((book) => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                      <select
-                        value={ book.shelf }
-                        onChange={(event) => this.handleSelectChange(event.target.value, book)}>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">{book.authors}</div>
-                </div>
-              </li>
-            ))
-            }
-          </ol>
+          <ListBooksGrid books={booksSearched} handleChange={this.props.handleChange}></ListBooksGrid>
         </div>
       </div>
     )
