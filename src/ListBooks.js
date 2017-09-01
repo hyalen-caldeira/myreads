9e4cd9ba65d9cb2d602d717a0a443daff7c71972
hyalen.moreira@gmail.com <hyalen.moreira@gmail.com>
2017-09-01 15:31:12 -0700

    Add support for array of objects instead of hardcode

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -11,21 +11,28 @@ class ListBooks extends Component {
   render() {
     const { books, handleChange } = this.props
 
+    const shelfType = [
+      {shelf : 'read', title : 'Read'},
+      {shelf : 'currentlyReading', title : 'Currently Reading'},
+      {shelf : 'wantToRead', title : 'Want to Read'}
+    ]
+
     return (
       <div className="list-books">
         <div className='list-books-title'>
           <h1>My Reads ...</h1>
         </div>
         <div>
-          <ListBooksShelf
-            books={books} shelf="read" title="Read" handleChange={handleChange}>
-          </ListBooksShelf>
-          <ListBooksShelf
-            books={books} shelf="currentlyReading" title="Currently Reading" handleChange={handleChange}>
-          </ListBooksShelf>
-          <ListBooksShelf
-            books={books} shelf="wantToRead" title="Want to Read" handleChange={handleChange}>
-          </ListBooksShelf>
+          {shelfType.map(type => {
+            return (
+              <ListBooksShelf
+                key={type.shelf}
+                books={books}
+                shelf={type.shelf}
+                title={type.title} handleChange={handleChange}>
+              </ListBooksShelf>
+            )
+          })}
         </div>
         <div className="open-search">
           <Link to='/search'>Search Books</Link>
